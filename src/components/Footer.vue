24607612e5c5965805b3d6bbd69004a5b0ef1f62
Andreas Kugel <andreas.kugel@web.de>
2026-03-04 23:37:36 +0100

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import gh from "@primer/octicons/build/svg/mark-github-16.svg"
+
+</script>
 <template>
     <div class="footer">
         <hr>
         <p>{{ $t('message.stay tuned') }}</p>
-        <p class="sourcecode">{{ $t('message.sourceCode') }} <a href="https://github.com/digital-codes/culturemap" target="_blank">github</a></p>
+        <p class="sourcecode">{{ $t('message.sourceCode') }} <a href="https://github.com/digital-codes/culturemap" target="_blank"><img :src="gh" alt="GitHub Logo" class="gh" ></a></p>
+        
     </div>
 </template>
 
@@ -11,5 +16,15 @@
     font-family: "Audiowide", cursive;
     text-align: center;
     color: var(--color-text);
+    display:inline-flex;
+    justify-content: center;
 }   
+.gh {
+    margin: 0;
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: white;
+}
 </style>
